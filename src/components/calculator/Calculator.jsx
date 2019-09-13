dca1ad6c578f1f480ad6d388025464ce068deb79
ygor <ygorpratta@gmail.com>
2019-09-13 14:04:24 -0300

    add structure folders

---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Calculator.scss'
+
+export default class Calculator extends Component{
+    render(){
+        return(
+            <div>
+                <h1>React Calculator</h1>
+                
+                <div className="cal">
+                 </div>
+            </div>
+        )
+    }
+}
